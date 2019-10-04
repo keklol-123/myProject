aa54230b3280a9e5a10b6fa7d95de10ff6598b4f
glebskr <glesbkr@gmail.com>
2019-10-04 17:48:24 +0300

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { App } from "./components/App";
+
+ReactDOM.render(
+    <App compiler="TypeScript" framework="React" />,
+    document.getElementById("example")
+);
+
+console.log(this)
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    mode: "production",
+    entry: "./src/index.tsx",
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: "index-bundle.js"
+      },
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js", ".jsx"]
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'awesome-typescript-loader'
+                    }
+                ]
+            },
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            }
+        ]
+    },
+
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: "./src/index.html"
+        })
+      ],
+
+};
