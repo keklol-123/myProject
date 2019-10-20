2a67454b2d3a180a35b8f0db07686c43bb0f7630
Gleb Skroba <glebskroba@MacBook-Pro-Gleb.local>
2019-10-20 14:05:40 +0300

    sync

--- server/User.js
+++ server/User.js
@@ -1,1 +1,17 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+
+const user = mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+    },
+    password: String,
+    links: [String],
+
+});
+
+const User = mongoose.model("User", user);
+
+
+
+module.exports = User;
--- server/index.js
+++ server/index.js
@@ -1,19 +1,32 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path")
- 
+
+const User = require('./User')
 const app = express();
 
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
 
+
 mongoose.connect(`mongodb+srv://gleb:glebgleb@mypetdb-mp6fc.mongodb.net/test?retryWrites=true&w=majority`,
 (err) => {
     if (err)
-        console.log(err)
+        console.log(err);
     else
-        console.log("Connected to DB")
+        console.log("Connected to DB");
 })
+
+const temp = new User({
+    email: 'mail@mail.ru',
+    password: '123346',
+    links: [
+        'yahoo.com',
+        'google.com'
+    ]
+}).save();
+
+
 
 app.use(express.static(__dirname + "/../public"));
  
