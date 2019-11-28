dff403597945b902345b53713eb6b9648395df91
glebskr <glebskr@gmail.com>
2019-11-28 20:08:42 +0300

    ts

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,8 @@
 import * as React from "react";
+import SignIn from './Auth/Auth';
 
-export interface HelloProps {
-  compiler: string;
-  framework: string;
+
+export const App = () => {
+ return <SignIn/>
 }
 
-export const App = (props: HelloProps) => (
-  <h1>
-    Hello from {props.compiler} and {props.framework}!
-  </h1>
-);
