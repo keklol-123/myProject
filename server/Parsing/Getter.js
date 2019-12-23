cd027fc0d05c37b9068b8997772733176eee0d3f
glebskr <glesbkr@gmail.com>
2019-12-23 23:42:34 +0300

    make it better

--- server/Parsing/Getter.js
+++ server/Parsing/Getter.js
@@ -1,7 +1,12 @@
 const puppeteer = require('puppeteer');
 
 const getHtml = async (link) => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+    ],
+  });
   const page = await browser.newPage();
   
   await page.goto(link);
